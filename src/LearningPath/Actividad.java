e25e6961873b8c231fd08d6eb69f47ef9e85197f
Julian Pinto <jr.pinto@uniandes.edu.co>
2024-10-20 10:12:21 -0500

    Primer commit

--- src/LearningPath/Actividad.java
+++ src/LearningPath/Actividad.java
@@ -1,5 +1,17 @@
 package LearningPath;
 
+import java.util.Date;
+
 public class Actividad {
+private String descripcion;
+private String objetivo;
+private String id;
+private Date fechaInicio;
+private Date fechaFin;
+private int duracion;
+private int dificultad;
+private double rating;
+private String tipoActividad;
+private boolean obligatoria;
 
 }
