06fa9cc80054c2d5cf4b8903ae0a2ca602a4f22e
Julian Pinto <jr.pinto@uniandes.edu.co>
2024-10-20 19:25:00 -0500

    Preguntas (falta encuesta)

--- src/Envio/PreguntaOpcionMultiple.java
+++ src/Envio/PreguntaOpcionMultiple.java
@@ -1,5 +1,26 @@
 package Envio;
 
-public class PreguntaOpcionMultiple {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreguntaOpcionMultiple extends Pregunta{
+	
+	public List<Opcion> opciones;
+
+	public PreguntaOpcionMultiple(String textoPregunta, int idPregunta) {
+		super(textoPregunta, idPregunta);
+		opciones= new ArrayList<Opcion>();
+		// TODO Auto-generated constructor stub
+	}
+	public void displayOpciones()
+	{
+		for (Opcion elemento: opciones) {
+			System.out.println("Opcion" + elemento.getIndice() + "----- "+ elemento.getDescripcion());
+		}
+	}
+	public List<Opcion> getOpciones()
+	{ 
+		return opciones;
+	}
 
 }
