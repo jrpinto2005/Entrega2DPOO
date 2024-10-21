e371f90a9c5c093705896bd6a8a99c34aa64b5c6
ASUS <ASUS@LAPTOP-CI4JBPDJ>
2024-10-20 21:56:10 -0500

    restrespo´s changes 2

--- src/Envio/EnvioRecurso.java
+++ src/Envio/EnvioRecurso.java
@@ -1,5 +1,11 @@
 package Envio;
 
-public class EnvioRecurso {
+import LearningPath.Actividad;
+
+public class EnvioRecurso extends Envio{
+
+	public EnvioRecurso(Actividad actividad, String idEstudiante, String tituloLp, boolean completado) {
+		super(actividad, idEstudiante, tituloLp, completado);
+	}
 
 }
