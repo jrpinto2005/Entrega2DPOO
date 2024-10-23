f37bda2e7e0a37c1ccd0c2b827e5a4f8d14a797f
jmrojasp1 <157617307+jmrojasp1@users.noreply.github.com>
2024-10-22 23:37:53 -0500

    Update MainCreacionLPyActividades.java
    primer main terminado

--- src/Usuarios/MainCreacionLPyActividades.java
+++ src/Usuarios/MainCreacionLPyActividades.java
@@ -26,7 +26,7 @@ public class MainCreacionLPyActividades {
         ConstructorQuiz q=new ConstructorQuiz();
         
 
-        // Crea algunos LearningPaths
+        // Crea LearningPaths
         LearningPath lp1 =c.crearLP("Java Básico", "Aprende los fundamentos de Java", 1, 40, 5, 
                                             new Date(), new Date(), 1, "profesor01", 
                                             "Entender lo básico de Java", 0.0);
@@ -36,7 +36,7 @@ public class MainCreacionLPyActividades {
                                             "Dominar conceptos avanzados de Java", 0.0);
     
         try {
-            // Crear actividad y agregarla al LearningPath
+            // Crea actividades
             Actividad act1 =  t.crearTarea("Video variables 1", "Introducir variables", "NombreActividad", 
                     new Date(), new Date(), 120, 3, 4.5, 
                     "Recurso Educativo", true, "Java Básico"); 
@@ -45,12 +45,12 @@ public class MainCreacionLPyActividades {
                     "Recurso Educativo", true, "Java Básico", 10); 
             System.out.println(act1.getDescripcion());
             System.out.println(act2.getDescripcion());
-           
+           //crea las opciones
             Opcion opcionI1=new Opcion("incorrecto", false, 1);
             Opcion opcionI2=new Opcion("incorrecto", false, 2);
             Opcion opcionI3=new Opcion("incorrecto", false, 3);
             Opcion opcionC=new Opcion("correcto", true, 4);
-            		
+            //crea la pregunta		
             PreguntaOpcionMultiple p = new PreguntaOpcionMultiple("Pregunta ejemplo",4);
             p.agregarOpcion(opcionI1);
             p.agregarOpcion(opcionI2);
